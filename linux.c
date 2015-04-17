43d9d76ec7a2a265dda23408366b112fe2bec30f
tauqeer729 <tauqeer729@gmail.com>
2015-04-18 04:14:32 +0530

    Commit message

---
+++ linux.c
@@ -0,0 +1,12 @@
+/* Program Description : A simple program to demonstrate usage of printf() in a tricky way.
+   Author : Mohammad Faisal Tauqeer
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s %s\n","Hello","Linux");
+	printf("%s : %s\n","Author","tauqeer.mf@gail.com");
+	return 0;
+}
